18e75cb7486616f0963c6834d8f7ec83543d38fd
bankiiz789 <sukkarin7303@gmail.com>
2024-06-12 17:28:33 +0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import catPic1 from "./assets/c1.png";
+import catPic2 from "./assets/c2.png";
+import sound from "./assets/sound.mp3";
 
-function App(props) {
+function App({ cat, handleDeleteCat, handleTotal, handleUpdate }) {
   // single cat
+  const [count, setCount] = useState(cat.count);
+  const [togglePicture, setTogglePicture] = useState(false);
+
+  const audioRef = useRef(null);
+
+  const playSound = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  const handleInc = () => {
+    setCount(count + 1);
+    setTogglePicture((prev) => !prev);
+    playSound();
+    handleTotal(1);
+    handleUpdate(cat.id, count + 1);
+  };
+  const handleDec = () => {
+    if (count > 0) {
+      setCount(count - 1);
+      setTogglePicture((prev) => !prev);
+      playSound();
+      handleTotal(-1);
+      handleUpdate(cat.id, count - 1);
+    }
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
 
   return (
     <div>
@@ -9,25 +42,49 @@ function App(props) {
         {/* แมว 1 ตัว */}
         <div className="flex justify-between items-center border border-gray-200 rounded-lg p-4 shadow-lg relative">
           {/* Remove Cat */}
-          <div className="absolute top-[-15px] right-[-15px] bg-red-400 text-white  w-[30px] h-[30px] rounded-full text-center align-middle cursor-pointer ">
+          <div
+            onClick={() => handleDeleteCat(cat.id)}
+            className="absolute top-[-15px] right-[-15px] bg-red-400 text-white  w-[30px] h-[30px] rounded-full text-center align-middle cursor-pointer "
+          >
             x
           </div>
           <div>
             {/* Cat Image */}
             <div className="w-[100px]">
-              <img className="w-full h-auto object-cover rounded-lg" src={""} />
+              <img
+                className="w-full h-auto object-cover rounded-lg"
+                src={togglePicture ? catPic2 : catPic1}
+              />
             </div>
           </div>
           {/* Cat Profile & Count*/}
           <div className="flex-1 px-4">
-            <p className="text-2xl">catName : Thailand</p>
-            <p>count : 0</p>
+            <p className="text-2xl">
+              {cat.catName} : {cat.country}
+            </p>
+            <p>count : {count}</p>
           </div>
           <div className="flex gap-1">
             {/* Button Group */}
-            <button className="px-4 py-2 bg-slate-500 rounded-md">reset</button>
-            <button className="px-4 py-2 bg-slate-500 rounded-md">-</button>
-            <button className="px-4 py-2 bg-slate-500 rounded-md">+</button>
+            <button
+              onClick={() => handleReset()}
+              className="px-4 py-2 bg-slate-500 rounded-md"
+            >
+              reset
+            </button>
+            <button
+              onClick={() => handleDec()}
+              className="px-4 py-2 bg-slate-500 rounded-md"
+            >
+              -
+            </button>
+            <button
+              onClick={() => handleInc()}
+              className="px-4 py-2 bg-slate-500 rounded-md"
+            >
+              +
+            </button>
+            <audio ref={audioRef} src={sound}></audio>
           </div>
         </div>
       </div>
--- src/Template.jsx
+++ src/Template.jsx
@@ -1,15 +1,65 @@
 import SplitScreen from "./layout/SplitScreen";
 import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import App from "./App";
 
 function Page() {
   // main page
+  const [allCats, setAllCats] = useState([]); // allCats.count
+  const [oneCat, setOneCat] = useState({ catName: "", country: "", count: 0 });
+  const [errorMessage, setErrorMessage] = useState("");
+  const [total, setTotal] = useState(0);
+
+  const handleChangeCatName = (e) => {
+    setOneCat({ ...oneCat, [e.target.name]: e.target.value });
+    // setErrorMessage("");
+  };
+
+  const handleUpdate = (id, newCount) => {
+    setAllCats((prev) =>
+      prev.map((cat) => (cat.id === id ? { ...cat, count: newCount } : cat))
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (oneCat.catName.trim() === "") {
+      setErrorMessage("Please enter your name");
+    } else if (oneCat.country === "") {
+      setErrorMessage("Please enter your country");
+    } else {
+      const newCat = { ...oneCat, id: nanoid() };
+      setAllCats((prev) => [...prev, newCat]);
+      setOneCat({ catName: "", country: "", count: 0 });
+      setErrorMessage("");
+    }
+  };
+
+  const handleDeleteCat = (id) => {
+    const newCatList = allCats.filter((cat) => cat.id !== id);
+    setAllCats(newCatList);
+  };
+
+  const handleTotal = (value) => {
+    setTotal(total + value);
+  };
+
+  const highestClick = Math.max(...allCats.map((cat) => cat.count), 0); // allCats.count [1,2,3] // Math.max([],0)
+
+  const leader = allCats.find((el) => el.count === highestClick);
+  // leader [] / .catName / undefined / maxCount /
+  // console.log(leader);
+
   return (
     <SplitScreen>
       {/******** Left Side  *********/}
       <>
         <div className="flex-1 flex flex-col p-[20px] text-center justify-center">
           {/******** Start Form ******/}
-          <form className="flex flex-col gap-y-5 p-8 border border-gray-300 rounded-md h-full justify-center text-black">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-y-5 p-8 border border-gray-300 rounded-md h-full justify-center text-black"
+          >
             <h1 className="text-3xl font-semibold italic text-red-400">
               Join for Click Cat !!
             </h1>
@@ -17,9 +67,17 @@ function Page() {
             <input
               className="w-full p-2 border-2 border-slate-300 rounded-md"
               placeholder="cat"
+              name="catName"
+              value={oneCat.catName}
+              onChange={handleChangeCatName}
             />
             {/* Cat Country */}
-            <select className="w-full p-2  border-2 border-slate-300 rounded-md">
+            <select
+              className="w-full p-2  border-2 border-slate-300 rounded-md"
+              name="country"
+              value={oneCat.country}
+              onChange={handleChangeCatName}
+            >
               <option value={""} disabled>
                 select country
               </option>
@@ -28,7 +86,12 @@ function Page() {
               <option value="UK">UK</option>
             </select>
             {/* Error */}
-            {<p className="text-red-500">error</p>}
+            {/* solution handle error 1 */}
+            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+            {/* solution handle error 2 */}
+            {/* {errorMessage ? (
+              <p className="text-red-500">{errorMessage}</p>
+            ) : null} */}
             {/* Submit */}
             <button
               type="submit"
@@ -46,23 +109,27 @@ function Page() {
             <div className="flex justify-evenly">
               <div>
                 <h1 className="text-3xl font-semibold italic">Total Cat</h1>
-                <h1 className="text-3xl font-semibold italic">0</h1>
+                <h1 className="text-3xl font-semibold italic">
+                  {allCats.length}
+                </h1>
               </div>
               <div>
                 <h1 className="text-3xl font-semibold italic">Total Count</h1>
-                <h1 className="text-3xl font-semibold italic">0</h1>
+                <h1 className="text-3xl font-semibold italic">{total}</h1>
               </div>
             </div>
             <div className="flex justify-evenly">
               <div>
                 <h1 className="text-3xl font-semibold italic">LEADER</h1>
                 <h1 className="text-3xl font-semibold italic">
-                  name : Thailand
+                  {leader?.catName} : {leader?.country}
                 </h1>
               </div>
               <div>
                 <h1 className="text-3xl font-semibold italic">Highest Click</h1>
-                <h1 className="text-3xl font-semibold italic">?</h1>
+                <h1 className="text-3xl font-semibold italic">
+                  {highestClick}
+                </h1>
               </div>
             </div>
           </div>
@@ -74,7 +141,15 @@ function Page() {
       {/******** Right Side  *********/}
       <div className="flex-1 h-screen p-4 flex flex-col gap-y-2 overflow-scroll">
         {/******** Start 1-Cat ******/}
-        <div>cat</div>
+        {allCats.map((el) => (
+          <App
+            key={el.id}
+            cat={el}
+            handleDeleteCat={handleDeleteCat}
+            handleTotal={handleTotal}
+            handleUpdate={handleUpdate}
+          />
+        ))}
         {/******** End 1-Cat ******/}
       </div>
     </SplitScreen>
